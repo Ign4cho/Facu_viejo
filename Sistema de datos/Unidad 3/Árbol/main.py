9148a49c66dcd1ff3ed8aa440db0e208eec59a90
Ign4cho <62896722+Ign4cho@users.noreply.github.com>
2023-05-03 16:10:21 -0300

    Update main.py

--- Sistema de datos/Unidad 3/Árbol/main.py
+++ Sistema de datos/Unidad 3/Árbol/main.py
@@ -2,13 +2,25 @@ from claseArbol import Arbol
 
 if __name__ == '__main__':
     a = Arbol()
-    a.insertar(10)
-    a.insertar(5)
-    a.insertar(7)
-    a.insertar(11)
-    a.insertar(15)
-    a.insertar(12)
+    a.iniciar(12,7,16,3,9,14,19,18,23)
+    print('Prueba muestra preorden, postorden e inorden:')
+    print(a.preorden(a.getRaiz()))
+    print(a.postorden(a.getRaiz()))
+    print(a.inorden(a.getRaiz()))
 
-    print(a.preorden(a.getRaiz(), ''))
-    print(a.postorden(a.getRaiz(), ''))
-    print(a.inorden(a.getRaiz(), ''))
+    print('\n\nPrueba hijo y camino:')
+    for i in range(25):
+        for j in range(25):
+            if a.padre(i, j):
+                print(f'{i} es padre de {j}')
+                print(a.camino(i,j))
+
+    print('\n\nPrueba Hoja')
+    for i in range(25):
+        if a.hoja(i):
+            print(f'{i} es hoja')
+
+    print('prueba nivel')
+    for i in range(25):
+        if a.buscar(i) is not None:
+            print(f'el nivel de {i} es {a.nivel(i)}')
