e651ffac07b378fd3cd8cf24e391f9741a46c67c
Ign4cho <62896722+Ign4cho@users.noreply.github.com>
2023-05-03 16:09:00 -0300

    Update claseArbol.py

--- Sistema de datos/Unidad 3/Árbol/claseArbol.py
+++ Sistema de datos/Unidad 3/Árbol/claseArbol.py
@@ -6,54 +6,121 @@ class Arbol:
     def __init__(self):
         self.__raiz = None
 
-    def insertar(self, n):          #todavía no lo pruebo
-        nuevo = Nodo(n)
-        if self.__raiz == None:
-            self.__raiz = nuevo
-        else:
-            aux = self.__raiz
-            bandera = False
-            while not bandera:  
-                if aux.getValor() > n:                  #si el n es menor al valor del nodo, vamos por izq
-                    auxiz = aux.getIz()
-                    if auxiz == None:
-                        aux.setIz(nuevo)
-                        bandera = True
-                    else:
-                        aux = auxiz
-                
-                elif aux.getValor() < n:                #si el n es mayor al valor del nodo, vamos por der
-                    auxde = aux.getDe()
-                    if aux.getDe() == None:
-                        aux.setDe(nuevo)
-                        bandera = True
+    def iniciar(self, *args):
+        for num in args:
+            self.inserta(self.__raiz, Nodo(num))
 
-                    else:
-                        aux = auxde
+    def inserta(self,nodo:Nodo, nuevo:Nodo):
+        if self.__raiz == None:             #para la primera inserción en la raíz
+            self.__raiz = Nodo(nuevo.getValor())
+        elif nuevo.getValor() < nodo.getValor():
+            if nodo.getIz() == None:
+                nodo.setIz(nuevo)
+            else:
+                self.inserta(nodo.getIz(), nuevo)
+        elif nuevo.getValor() > nodo.getValor():
+            if nodo.getDe() == None:
+                nodo.setDe(nuevo)
+            else:
+                self.inserta(nodo.getDe(), nuevo)
+        else: 
+            nodo.setCuenta(1)
+    
+    def hijo(self, hijo: int, padre:int): #unico problema, si ingresan dos veces la misma clave retorna true
+        npadre = self.buscar(padre)
+        if hijo == padre or npadre == None or self.hoja(padre):
+            return False
+        elif hijo < padre:
+            iz = npadre.getIz()
+            if iz is not None:
+                if iz.getValor() == hijo:
+                    return True
                 else:
-                    aux.setCuenta(1)                    #si es igual, incrementamos contador
-                    bandera = True
+                    return self.hijo(hijo, iz.getValor())
+            else:
+                return False
+        elif hijo > padre:
+            de = npadre.getDe()
+            if de is not None:
+                if de.getValor() == hijo:
+                    return True
+                else:
+                    return self.hijo(hijo, de.getValor())
+            else:
+                return False
+    
+    def padre(self, padre: int, hijo:int):
+        return self.hijo(hijo, padre)
 
-    def preorden(self, nodo:Nodo, re: str):
-        re += f'{nodo.getValor()}-'
+    def buscar(self, valor):
+        ret = None
+        actual = self.__raiz
+        while ret == None and actual is not None:
+            if valor == actual.getValor():
+                ret = actual
+            elif valor < actual.getValor():
+                actual = actual.getIz()
+            else:
+                actual = actual.getDe()
+        return ret
+        
+    def hoja(self, valor):
+        n = self.buscar(valor)
+        if n == None:
+            ret = None          #no existe el nodo
+        else:
+            if n.getIz() == None and n.getDe() == None:
+                ret = True
+            else:
+                ret = False
+        return ret  
+    
+    def camino(self, padre, hijo):
+        if self.hijo(hijo,padre):
+            if hijo < padre:
+                return str(self.buscar(padre).getValor())+'-'+ self.camino(self.buscar(padre).getIz().getValor(), hijo)
+            else:
+                return str(self.buscar(padre).getValor())+'-'+ self.camino(self.buscar(padre).getDe().getValor(), hijo)
+        else:
+            return str(hijo)
+        
+    def nivel(self, x):
+        actual = self.__raiz
+        ret = 0
+        if x == self.__raiz.getValor():
+            pass
+        elif self.hijo(x, self.getRaiz().getValor()):
+            while actual.getValor() != x:
+                if actual.getValor() > x:
+                    actual = actual.getIz()
+                else:
+                    actual = actual.getDe()
+                ret +=1
+        else:
+            ret = None
+        return ret
+    
+    
+    def preorden(self, nodo:Nodo, re=''):
+        re += f'{nodo.getValor()},'
         if nodo.getIz() != None:
             re = self.preorden(nodo.getIz(), re)
         if nodo.getDe() != None:
             re = self.preorden(nodo.getDe(), re)
         return re
    
-    def postorden(self, nodo:Nodo, re: str):
+    def postorden(self, nodo:Nodo, re=''):
         if nodo.getIz() != None:
             re = self.postorden(nodo.getIz(), re)
         if nodo.getDe() != None:
             re = self.postorden(nodo.getDe(), re)
-        re += f'{nodo.getValor()}-'
+        re += f'{nodo.getValor()},'
         return re
     
-    def inorden(self, nodo: Nodo, re: str):
+    def inorden(self, nodo: Nodo, re=''):
         if nodo.getIz() != None:
             re = self.inorden(nodo.getIz(), re)
-        re += f'{nodo.getValor()}-'
+        re += f'{nodo.getValor()},'
         if nodo.getDe() != None:
             re = self.inorden(nodo.getDe(), re)
         return re
